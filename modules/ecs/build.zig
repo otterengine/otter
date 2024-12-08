afc0367b3f479ebaedc05ea6cf360403a67a9f22
Olle Lukowski <ollelukowski@hotmail.com>
2024-12-08 13:55:43 +0100

    chore: :sparkles: initialize the ECS module

--- modules/ecs/build.zig
+++ modules/ecs/build.zig
@@ -1,27 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
 
-    const mod = b.addModule("otter", .{
+    _ = b.addModule("ecs", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const ecs_dep = b.dependency("ecs", .{});
-    const ecs = ecs_dep.module("ecs");
-
-    mod.addImport("ecs", ecs);
 
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    unit_tests.root_module.addImport("ecs", ecs);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&unit_tests.step);
